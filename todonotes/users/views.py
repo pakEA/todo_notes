22442042fe6bb547b7f75323272371380ab4d3bb
pakEA <pak_ea@inbox.ru>
2022-03-18 16:54:23 +0300

    replaced ModelViewSet

--- todonotes/users/views.py
+++ todonotes/users/views.py
@@ -1,9 +1,10 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import UpdateModelMixin
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from .models import Users
 from .serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+class UserModelViewSet(UpdateModelMixin, ReadOnlyModelViewSet):
     queryset = Users.objects.all()
     serializer_class = UserModelSerializer
