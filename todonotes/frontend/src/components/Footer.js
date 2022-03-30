e01f6dbb82d290b909b5ac6ae0fc59a61580ec82
pakEA <pak_ea@inbox.ru>
2022-03-30 14:40:54 +0300

    fix

--- todonotes/frontend/src/components/Footer.js
+++ todonotes/frontend/src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="panel-footer">
-            <div className="container">
-                <p className="text-muted text-center">All right reversed</p>
+        <div className={"panel-footer"}>
+            <div className={"container"}>
+                <p className={"text-muted text-center"}>All right reversed</p>
             </div>
         </div>
     )
