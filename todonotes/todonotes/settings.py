4263ddc76bbfde2d747cb8f920808634ceabac7b
pakEA <pak_ea@inbox.ru>
2022-04-05 13:00:54 +0300

    + permissions

--- todonotes/todonotes/settings.py
+++ todonotes/todonotes/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     ############
     'rest_framework',
+    'rest_framework.permissions',
     'corsheaders',
     'django_filters',
     ############
@@ -148,6 +149,9 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
