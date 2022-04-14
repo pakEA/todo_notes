c8eb850d527197dca709fd688611c4a245d40a07
pakEA <pak_ea@inbox.ru>
2022-04-14 10:59:09 +0300

    + fix

--- todonotes/frontend/src/components/Projects.js
+++ todonotes/frontend/src/components/Projects.js
@@ -12,35 +12,9 @@ const ProjectItem = ({project}) => {
 }
 
 
-class ProjectList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            projects: [],
-            isLoaded: false
-        }
-    }
-
-    componentDidMount() {
-        fetch('http://127.0.0.1:8000/api/projects/')
-            .then(response => response.json())
-            .then((json) => {
-                this.setState({
-                    isLoaded: true,
-                    projects: json.results
-                })
-            }).catch(error => console.log(error))
-    }
-
-    render() {
-        const {isLoaded, projects} = this.state;
-        if(!isLoaded) {
-            return <div>
-                <h1>Loading...</h1>
-            </div>
-        }
-        return (
-            <div className={"container"}>
+const ProjectList = ({projects}) => {
+    return (
+        <div className={"container"}>
             <table className={"table table-bordered table-hover"}>
                 <thead className={"table-light"}>
                     <tr>
@@ -52,9 +26,52 @@ class ProjectList extends React.Component {
                 {projects.map((project) => <ProjectItem project={project}/>)}
             </table>
         </div>
-        )
-    }
+    )
 }
+
+// class ProjectList extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             projects: [],
+//             isLoaded: false
+//         }
+//     }
+//
+//     componentDidMount() {
+//         fetch('http://127.0.0.1:8000/api/projects/')
+//             .then(response => response.json())
+//             .then((json) => {
+//                 this.setState({
+//                     isLoaded: true,
+//                     projects: json.results
+//                 })
+//             }).catch(error => console.log(error))
+//     }
+
+    // render() {
+        // const {isLoaded, projects} = this.state;
+        // if(!isLoaded) {
+        //     return <div>
+        //         <h1>Loading...</h1>
+        //     </div>
+        // }
+//         return (
+//             <div className={"container"}>
+//             <table className={"table table-bordered table-hover"}>
+//                 <thead className={"table-light"}>
+//                     <tr>
+//                         <th>Project name</th>
+//                         <th>Description</th>
+//                         <th>Users</th>
+//                     </tr>
+//                 </thead>
+//                 {projects.map((project) => <ProjectItem project={project}/>)}
+//             </table>
+//         </div>
+//         )
+//     }
+// }
 
 
 export default ProjectList;
