546fea353f7992d73020ac6adbee2d7e8fadda87
pakEA <pak_ea@inbox.ru>
2022-04-18 17:31:30 +0300

    + versioning class

--- todonotes/todonotes/settings.py
+++ todonotes/todonotes/settings.py
@@ -159,5 +159,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.QueryParameterVersioning',
 }
