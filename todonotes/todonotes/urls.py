b74a3c1d9238b9225856613d1ab7fff8611d4a34
pakEA <pak_ea@inbox.ru>
2022-04-18 17:30:01 +0300

    + api/users/

--- todonotes/todonotes/urls.py
+++ todonotes/todonotes/urls.py
@@ -1,20 +1,5 @@
-"""todonotes URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 
@@ -23,7 +8,7 @@ from users.views import UserModelViewSet
 
 
 router = DefaultRouter()
-router.register('users', UserModelViewSet)
+# router.register('users', UserModelViewSet)
 router.register('projects', ProjectModelViewSet)
 router.register('todo_notes', ToDoModelViewSet)
 
@@ -31,5 +16,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('api/users/', UserModelViewSet.as_view()),
     path('api-token-auth/', views.obtain_auth_token),
 ]
